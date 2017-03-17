640998735eeff62d558b2e8384edd9fe84b73605
Guido Chari <charig@gmail.com>
2017-03-17 20:37:51 -0300

    Some more work on setup scripts

--- scripts/setupRepo.sh
+++ scripts/setupRepo.sh
@@ -6,8 +6,15 @@ SCRIPT_PATH=`dirname $0`;
 source $SCRIPT_PATH/basicFunctions.inc
 
 IMAGE_DIR="$SCRIPT_PATH/../image"
+BASE_DIR="$SCRIPT_PATH/.."
 
-bash checkoutVMMaker.sh
-bash newImage.sh "loadSqueakNOSVM.st
-bash newImage.sh "loadSqueakNOSImage.st"
-bash installImage.sh
+pushd $BASE_DIR
+    git submodule update --init --recursive
+popd > /dev/null
+
+pushd $SCRIPT_PATH
+    bash checkoutVMMaker.sh
+    bash newImageWithProjectLoaded.sh "loadSqueakNOSVM.st"
+    bash newImageWithProjectLoaded.sh "loadSqueakNOSImage.st"
+    bash installImage.sh
+popd > /dev/null    
