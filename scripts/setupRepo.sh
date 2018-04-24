a800f48609352997c3ab3a0bcedd0f6f45a805d7
Guido Chari <charig@gmail.com>
2018-04-24 15:38:39 +0200

    git files make lot of dirs to appear because of a bug? in sparse checkout

--- scripts/setupRepo.sh
+++ scripts/setupRepo.sh
@@ -22,9 +22,9 @@ OK "Submodules initialized"
 INFO "Configuring Sparse checkout for the submodules"
 cat > "$BASE_DIR/.git/modules/opensmalltalk-vm/info/sparse-checkout" << EOF
 spur64src
-third-party
 platforms/nopsys
 platforms/Cross
+./third-party/
 image/envvars.sh
 image/CogNOS Generation Workspace.text
 image/BuildSqueakSpurTrunkVMMakerImage.st
