006dee758966f163ff8626882ad74df5c70c2a7c
Guido Chari <charig@gmail.com>
2018-05-29 16:41:46 +0200

    Add scripts for building the sources

---
+++ scripts/buildSources.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+source `dirname $0`/basicFunctions.inc
+
+BASE_DIR=".."
+VM_DEV_DIR="$BASE_DIR/opensmalltalk-vm/image"
+
+pushd $VM_DEV_DIR
+source get64VMName.sh
+
+echo $VM
+
+INFO "Generating Sources"
+if [ "$1" = "interpreter" ]    
+then
+    $VM $BASE.image ../../scripts/Smalltalk/buildInterpreterSources.st
+else  
+    $VM $BASE.image ../../scripts/Smalltalk/buildJitSources.st
+fi
+
+OK "done"
+popd > /dev/null
+
