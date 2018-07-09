30d7c4a096f55664e10c3975d6b36b718e4dc962
Guido Chari <charig@gmail.com>
2018-07-09 17:35:49 +0200

    Port bash options to ENV variables so that it is easier to pass them from bash to makefiles

--- scripts/buildSources.sh
+++ scripts/buildSources.sh
@@ -8,13 +8,14 @@ VM_DEV_DIR="$BASE_DIR/opensmalltalk-vm/image"
 pushd $VM_DEV_DIR
 source get64VMName.sh
 
-if [ "$1" = "interpreter" ]    
+if [ ! -z "$JIT" ]
 then
-	INFO "Generating Interpreter Sources"
-    $VM -headless $BASE64.image ../../scripts/Smalltalk/buildInterpreterSources.st
-else  
-	INFO "Generating JIT Sources"
+    
+    INFO "Generating JIT Sources"
     $VM -headless $BASE64.image ../../scripts/Smalltalk/buildJitSources.st
+else  
+    INFO "Generating Interpreter Sources"
+    $VM -headless $BASE64.image ../../scripts/Smalltalk/buildInterpreterSources.st
 fi
 
 OK "done"
