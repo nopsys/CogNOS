8e6d65d5bb38a396f8f67ce0d1b01709400c80e1
Javier Pimás <elpochodelagente@gmail.com>
2018-07-10 03:59:01 -0300

    slang translation script just generates both interpreter and jit

--- scripts/buildSources.sh
+++ scripts/buildSources.sh
@@ -8,15 +8,9 @@ VM_DEV_DIR="$BASE_DIR/opensmalltalk-vm/image"
 pushd $VM_DEV_DIR
 source get64VMName.sh
 
-if [ ! -z "$JIT" ]
-then
-    
-    INFO "Generating JIT Sources"
-    $VM -headless $BASE64.image ../../scripts/Smalltalk/buildJitSources.st
-else  
-    INFO "Generating Interpreter Sources"
-    $VM -headless $BASE64.image ../../scripts/Smalltalk/buildInterpreterSources.st
-fi
+INFO "Generating Both Interpreter and JIT Sources"
+$VM -headless $BASE64.image ../../scripts/Smalltalk/buildInterpreterSources.st
+$VM -headless $BASE64.image ../../scripts/Smalltalk/buildJitSources.st
 
 OK "done"
 popd > /dev/null
