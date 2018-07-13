5f9de11504adac4dd8423a3e8a1585c4e6be7057
Javier Pimás <elpochodelagente@gmail.com>
2018-07-13 17:55:57 -0300

    improve release scripts, take 5

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,10 +12,10 @@ pushd $NOPSYS_DIR
 make clean
 if [[ -z "$VERSION"  ||  "$VERSION" == "HD" ]]
 then
-    make build/vmware.hd.vmx
+    make -C ../../../nopsys build/vmware.hd.vmx
     make hd
 else
-    make build/vmware.cd.vmx
+    make -C ../../../nopsys build/vmware.cd.vmx
     make iso
 fi
 popd > /dev/null
