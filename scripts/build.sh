a491b6a480d24efea4086bf6838061b40107a707
Guido Chari <charig@gmail.com>
2018-08-29 14:26:54 +0200

    More work to make scripts path independent

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 set -e
-source `dirname $0`/basicFunctions.inc
 
-BASE_DIR=".."
+SCRIPT_PATH="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"
+if [ ! -d $SCRIPT_PATH ]; then
+    echo "Could not determine absolute dir of $0"
+    echo "Maybe accessed with symlink"
+fi
+
+
+source $SCRIPT_PATH/basicFunctions.inc
+
+BASE_DIR="$SCRIPT_PATH/.."
 NOPSYS_DIR="$BASE_DIR/opensmalltalk-vm/platforms/nopsys"
 
-./buildSources.sh $@
-
+$SCRIPT_PATH/buildSources.sh $@
 
 pushd $NOPSYS_DIR
 make clean
--- scripts/buildSources.sh
+++ scripts/buildSources.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 set -e
-source `dirname $0`/basicFunctions.inc
 
-BASE_DIR=".."
+SCRIPT_PATH="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"
+if [ ! -d $SCRIPT_PATH ]; then
+    echo "Could not determine absolute dir of $0"
+    echo "Maybe accessed with symlink"
+fi
+
+source $SCRIPT_PATH/basicFunctions.inc
+
+BASE_DIR="$SCRIPT_PATH/.."
 VM_DEV_DIR="$BASE_DIR/opensmalltalk-vm/image"
 
 pushd $VM_DEV_DIR
